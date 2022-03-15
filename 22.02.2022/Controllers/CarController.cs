38a1fe80f5ec89e31614822bdc29458001711ca6
PC07-204 <PC07-204@DESKTOP-P9N6NDN>
2022-03-15 10:46:31 +0200

    Add project files.

---
+++ 22.02.2022/Controllers/CarController.cs
@@ -0,0 +1,20 @@
+﻿using _22._02._2022.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._02._2022.Controllers
+{
+    class CarController
+    {
+       public List<Car> GetAllCars()
+        {
+            using (CarsDBEntities ex = new CarsDBEntities())
+            {
+                return ex.Cars.ToList();
+            }
+        }
+    }
+}
---
+++ 22.02.2022/Views/MainView.cs
@@ -0,0 +1,28 @@
+﻿using _22._02._2022.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22._02._2022.Views
+{
+    public partial class MainView : Form
+    {
+        CarController carcontroller = new CarController();
+
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void MainView_Load(object sender, EventArgs e)
+        {
+            dgvCars.DataSource = carcontroller.GetAllCars();
+        }
+    }
+}
